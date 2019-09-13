7be92cdfbcda219c71f919d05ec66e8ed8563f88
BrettBerg316 <54595160+BrettBerg316@users.noreply.github.com>
2019-09-13 17:00:36 -0500

    hello-world.c
    A hello-world program (with a twist)

---
+++ hello-world.c
@@ -0,0 +1,16 @@
+/*
+ * Author: Brett Berg & Markus Kittlaus
+ * Date: 13/09/19
+ *
+ * A simple hello world program (with a twist)
+ *
+ */
+ 
+#include <stdio.h>
+
+int main () {
+	
+	printf("Brett Berg and Markus Kittlaus");
+	return 0
+	
+}
